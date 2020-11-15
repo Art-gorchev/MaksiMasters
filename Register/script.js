889164767134ac34fa133df59f541d5e730008a5
Artem <68824098+Art-gorchev@users.noreply.github.com>
2020-11-15 11:44:21 +0300

    MaksiMaster
    MaksiMaster

---
+++ Register/script.js
@@ -0,0 +1,13 @@
+$(".info-item .btn").click(function(){
+  $(".container").toggleClass("log-in");
+});
+$(".container-form .btn").click(function(){
+  $(".container").addClass("active");
+});
+
+$(".info-item .btn-1").click(function(){
+  $(".container").toggleClass("log-in");
+});
+$(".container-form .btn-1").click(function(){
+  $(".container").addClass("active");
+});
